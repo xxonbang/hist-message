45bb1cd4f277c3d39d385563ae927c4c287ce59e
xxonbang <bson@hist.co.kr>
2020-11-13 14:57:07 +0900

    - 모바일 화면 관련 작업 1. index.html 의 meta 수정 2. 일부 css 수정

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
